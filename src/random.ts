7a0def394ded0b29a58562b3cafa219fe25f6d7e
Ben Cutler <ben@titanvest.com>
2021-12-10 09:22:37 -0500

    fix

--- src/random.ts
+++ src/random.ts
@@ -1,6 +1,17 @@
+import database from "./data-layer/database";
 import ghinApi from "./networking/ghin-api";
 import logger from "./util/logger";
 import persistCourses from "./workers/persist-courses";
 
-persistCourses.run()
+
+
   
+
+const scriptToRun = async () => {
+  await database.startDB()
+  // insert here below here
+  await persistCourses.run()
+}
+
+scriptToRun()
+  .then(() => logger.info('done running rando'))
